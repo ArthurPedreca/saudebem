d35a6c86ab0619fb6ce552191ad2de859c9a7aa7
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-08 10:03:43 -0300

    at3tri finalizada

--- saudebem.js
+++ saudebem.js
@@ -1,94 +1,75 @@
-// Requisitando os módulos
-const mongoose = require('mongoose');
 const express = require('express');
-const bodyParser = require('body-parser');
-
-// Configurando o Express
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const mongoose = require('mongoose');
 
-const port = 3000;
-
-// Configurando o banco de dados
+// Conectar-se ao banco de dados MongoDB
 mongoose.connect('mongodb://127.0.0.1:27017/saudebem', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 10000,
 });
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', console.error.bind(console, 'Erro na conexão com o MongoDB'));
+
 db.once('open', function () {
-  console.log('Estamos conectados ao MongoDB!');
+  console.log('Conectado ao MongoDB');
 });
 
-// Criando a model do projeto para Produtos
 const produtoSchema = new mongoose.Schema({
-  id: { type: String, required: true },
-  descricao: { type: String },
-  lab: { type: String },
-  validade: { type: Number },
-  estoque: { type: String },
-});
+    id: { type: String, required: true },
+    descricao: { type: String },
+    lab: { type: String },
+    validade: { type: Number },
+    estoque: { type: String },
+  });
 
 const Produto = mongoose.model('Produto', produtoSchema);
 
-// Criando a model do projeto para Usuários
-const usuarioSchema = new mongoose.Schema({
-  nome: { type: String, required: true },
-  senha: { type: String, required: true },
+// Configurar o Express para usar arquivos estáticos
+app.use(express.static(__dirname));
+
+
+app.get('/', (req, res) => {
+  // Rota para a página HTML do formulárioZS
+  res.sendFile(__dirname + '/index.html');
 });
 
-const Usuario = mongoose.model('Usuario', usuarioSchema);
+app.post('/cadastrarprodutoentrega', (req, res) => {
+  // Rota para lidar com o envio do formulário
+  let formData = '';
 
-// Configuração dos roteamentos
-// Cadastrar Produto
-app.post('/cadastrarproduto', async (req, res) => {
-  const { id, descricao, lab, validade, estoque } = req.body;
-
-  // Criando uma nova instância do Produto com os dados recebidos
-  const produto = new Produto({
-    id,
-    descricao,
-    lab,
-    validade,
-    estoque,
+  req.on('data', (chunk) => {
+    formData += chunk;
   });
 
-  try {
-    // Tenta salvar o novo produto no banco de dados
-    await produto.save();
-    // Se bem-sucedido, envia uma resposta de status 201 (Criado)
-    res.status(201).json({ message: 'Produto cadastrado com sucesso!' });
-  } catch (error) {
-    // Se houver erro, envia uma resposta de erro
-    res.status(500).json({ message: 'Erro ao cadastrar produto', error: error.message });
-  }
-});
+  req.on('end', () => {
+    const parsedData = new URLSearchParams(formData);
 
-// Cadastrar Usuário
-app.post('/cadastrarusuario', async (req, res) => {
-  const { nome, senha } = req.body;
+    const novoUsuario = new Produto({
+        id: parsedData.get('produtoId'),
+        descricao: parsedData.get('descricao'),
+        lab: parsedData.get('laboratorio'),
+        validade: parsedData.get('dataValidade'),
+        estoque: parsedData.get('quantidadeEstoque'),
+    });
 
-  const usuario = new Usuario({
-    nome,
-    senha,
+    novoUsuario
+      .save()
+      .then(() => {
+        console.log('Usuário salvo com sucesso no banco de dados.');
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Usuário salvo com sucesso.');
+      })
+      .catch((err) => {
+        console.error('Erro ao salvar o usuário:', err);
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Erro ao salvar o usuário.');
+      });
   });
-
-  try {
-    // Tenta salvar o novo usuário no banco de dados
-    await usuario.save();
-    // Se bem-sucedido, envia uma resposta de status 201 (Criado)
-    res.status(201).json({ message: 'Usuário cadastrado com sucesso!' });
-  } catch (error) {
-    // Se houver erro, envia uma resposta de erro
-    res.status(500).json({ message: 'Erro ao cadastrar usuário', error: error.message });
-  }
 });
 
-// Iniciando o servidor
+const port = 3000;
 app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
+  console.log(`Servidor está rodando em http://localhost:${port}/`);
 });
